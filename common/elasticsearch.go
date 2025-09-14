ff6d7e0941ea34ccab9e1b24fb0cd388658fd03b
ndtrong <nguyenductrong12a1@gmail.com>
2025-09-14 14:11:57 +0700

    feat: add es connection

--- common/elasticsearch.go
+++ common/elasticsearch.go
@@ -2,10 +2,32 @@ package common
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 
+	"github.com/elastic/go-elasticsearch/v9"
 	"github.com/elastic/go-elasticsearch/v9/typedapi/core/search"
+	"github.com/tripconnect/go-common-utils/helper"
 )
+
+var ElasticsearchClient *elasticsearch.TypedClient
+
+func init() {
+	host, hostErr := helper.ReadConfig[string]("database.elasticsearch.host")
+
+	if hostErr != nil {
+		log.Fatalf("failed to load elasticsearch config")
+	}
+
+	var err error
+	ElasticsearchClient, err = elasticsearch.NewTypedClient(elasticsearch.Config{
+		Addresses: []string{fmt.Sprintf("http://%s:9200", host)},
+	})
+
+	if err != nil {
+		log.Fatalf("Error creating the Elasticsearch client: %s", err)
+	}
+}
 
 func GetResponseDocs[T any](resp *search.Response) []T {
 	var docs []T
